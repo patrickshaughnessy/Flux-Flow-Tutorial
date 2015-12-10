8a177f9dd331f449878c2b5978a245d60506a9f0
Darien Lombardi <darien.lombardi@gmail.com>
2015-12-09 23:02:21 -0800

    add AppController

--- public/lib/components/AppController.js
+++ public/lib/components/AppController.js
@@ -1,8 +1,17 @@
 import React from "react";
 
-class AppController extends React.Component({
+class AppController extends React.Component{
   constructor(props){
     super(props);
     this.state = {  }
   }
-})
+  render(){
+    return(
+      <div className="app">
+        Hello World!
+      </div>
+    )
+  }
+}
+
+export default AppController
